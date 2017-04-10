7630bea64bd78bf23b0d3be1be607ef133a5d093
Ritesh Kumar <rkritesh078@gmail.com>
2017-04-10 23:59:51 +0530

    implemented basic structure

--- cli.js
+++ cli.js
@@ -1,7 +1,38 @@
 #!/usr/bin/env node
 'use strict';
 const meow = require('meow');
-const starring = require('.');
+const ghAuth = require('ghauth');
+var loadPkg = require('load-pkg');
+const npmToGitHub = require('npm-to-github');
+const pSettle = require('p-settle');
+const ghGot = require('gh-got');
+
+function getPackageNames(pkg) {
+	const deps = Object.assign({}, pkg.dependencies, pkg.devDependencies);
+	return Object.keys(deps);
+}
+
+async function getPackageRepoNames(pkg) {
+	const repoNames = getPackageNames(pkg).map(packageName => npmToGitHub.getRepositoryName(packageName));
+
+	const results = await pSettle(repoNames)
+	return results.filter(repo => repo.isFulfilled).map(repo => repo.value)
+}
+
+async function starRepos(token, pkg) {
+	const packageRepoNames = await getPackageRepoNames(pkg);
+
+	const mapper = packageRepoNames.map(repoName => ghGot(`user/starred/${repoName}`, {token, method:'PUT'}))
+
+	const response = await pSettle(mapper)
+	console.log('DONE', response)
+}
+
+function init(token) {
+	loadPkg(async function(err, pkg) {
+		starRepos(token, pkg)
+	});
+}
 
 const cli = meow(`
 	Usage
@@ -17,4 +48,13 @@ const cli = meow(`
 	  ponies & rainbows
 `);
 
-console.log(starring(cli.input[0] || 'unicorns'));
+var opts = {
+	configName : 'awesome'
+        , scopes     : [ 'public_repo' ]
+        , note       : 'This token is for my awesome app'
+        , userAgent  : 'My Awesome App'
+}
+
+ghAuth(opts, async function (err, authData) {
+	init(authData.token)
+})
